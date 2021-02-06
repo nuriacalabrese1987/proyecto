ae597b2568ac1c2569c4432684c73480e2741b95
nuriacalabrese1987 <56678736+nuriacalabrese1987@users.noreply.github.com>
2021-02-06 10:15:05 +0100

    nuria

---
+++ ProyectoGmq/app/src/main/java/com/gmq/proyectogmq/FotoActivity.java
@@ -0,0 +1,14 @@
+package com.gmq.proyectogmq;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto);
+    }
+}
--- ProyectoGmq/app/src/main/java/com/gmq/proyectogmq/LoginActivity.java
+++ ProyectoGmq/app/src/main/java/com/gmq/proyectogmq/LoginActivity.java
@@ -1,4 +1,25 @@
 package com.gmq.proyectogmq;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dos = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(dos);
+            }
+        }, 2000);
+    }
 }
