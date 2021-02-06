8c8673b287e0d8fc5f860f6f5cf4e8f94bd88fb2
nuriacalabrese1987 <56678736+nuriacalabrese1987@users.noreply.github.com>
2021-02-06 15:21:15 +0100

    nuria

--- ProyectoGmq/app/src/main/java/com/gmq/proyectogmq/LoginActivity.java
+++ ProyectoGmq/app/src/main/java/com/gmq/proyectogmq/LoginActivity.java
@@ -3,6 +3,7 @@ package com.gmq.proyectogmq;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,13 +14,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent dos = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(dos);
-            }
-        }, 2000);
+
     }
+
+    public void token(View view){
+        Intent token = new Intent(LoginActivity.this, TokenActivity.class );
+        startActivity(token);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
 }
