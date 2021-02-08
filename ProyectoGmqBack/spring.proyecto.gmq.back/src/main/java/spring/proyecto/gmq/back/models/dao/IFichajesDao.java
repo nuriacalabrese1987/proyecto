a001d3aa43dd41dbf0dc2274ee755b2dfdfd10d9
Ignacio Campo Martin <icampom1996@gmail.com>
2021-02-08 19:55:08 +0100

    Organizacion clases

--- ProyectoGmqBack/spring.proyecto.gmq.back/src/main/java/spring/proyecto/gmq/back/models/dao/IFichajesDao.java
+++ ProyectoGmqBack/spring.proyecto.gmq.back/src/main/java/spring/proyecto/gmq/back/models/dao/IFichajesDao.java
@@ -1,11 +1,17 @@
 package spring.proyecto.gmq.back.models.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 import spring.proyecto.gmq.back.models.entity.Fichajes;
 
 
-
+@EnableJpaRepositories
 public interface IFichajesDao extends CrudRepository<Fichajes, Long>{
 
+	@Query("select u from Fichajes u where u.id_empleado =?1")
+	public List<Fichajes> findAllFichajesEmpleado(Long id);
 }
