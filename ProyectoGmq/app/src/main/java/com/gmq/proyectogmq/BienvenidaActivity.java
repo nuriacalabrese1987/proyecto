e5960a82175be3d08e2f2256295f35b41f596dd7
nuriacalabrese1987 <56678736+nuriacalabrese1987@users.noreply.github.com>
2021-02-09 11:47:10 +0100

    nuria

---
+++ ProyectoGmq/app/src/main/java/com/gmq/proyectogmq/BienvenidaActivity.java
@@ -0,0 +1,15 @@
+package com.gmq.proyectogmq;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BienvenidaActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
